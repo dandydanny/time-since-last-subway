d7eb5483bda732b90f8c5754cde1100f0ac8629d
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-01 23:32:47 -0500

    jquery scale explosion

--- counter.js
+++ counter.js
@@ -1,28 +1,36 @@
-var hoursLabel = document.getElementById("hours");
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
+$(document).ready(function() {
 
-setInterval(setTime, 1000);
+  var hoursLabel = document.getElementById("hours");
+  var minutesLabel = document.getElementById("minutes");
+  var secondsLabel = document.getElementById("seconds");
+  var totalSeconds = 0;
+  
+  setInterval(setTime, 1000);
+  
+  totalSeconds = 0;
+  
+     function setTime() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds%60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds/60%60));
+        hoursLabel.innerHTML = pad(parseInt(totalSeconds/3600));
+     }
+  
+     function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+           return "0" + valString;
+        } else {
+           return valString;
+        }
+     }
+  
+     $( ".digit").on( "click", function() {
+      resetTime();
+      $( "#sandwich" ).show("scale", "percent: 500", 4000, null);
+    });
 
-totalSeconds = 0;
-
-   function setTime() {
-      ++totalSeconds;
-      secondsLabel.innerHTML = pad(totalSeconds%60);
-      minutesLabel.innerHTML = pad(parseInt(totalSeconds/60%60));
-      hoursLabel.innerHTML = pad(parseInt(totalSeconds/3600));
-   }
-
-   function pad(val) {
-      var valString = val + "";
-      if (valString.length < 2) {
-         return "0" + valString;
-      } else {
-         return valString;
-      }
-   }
-
-   function resetTime() {
-      totalSeconds = 0;
-   }
+     function resetTime() {
+        totalSeconds = 0;
+     }
+});
